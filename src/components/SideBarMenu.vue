b42d61175d7ee3b467737f97ef0c435d565b5e07
BorjGD <borjagarciadelgado9@gmail.com>
2022-02-15 23:57:12 +0000

    Add new component SideBarMenu
    Add new component SideBarMenu to show navigatio links in mobile devices

---
+++ src/components/SideBarMenu.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="side-bar">
+    <nav class="side-bar-nav">
+      <ul class="side-bar-links">
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+      </ul>
+    </nav>
+  </div>
+</template>
+<script>
+export default {
+  name: "SideBarMenu",
+};
+</script>
+<style scoped>
+.side-bar {
+  background-color: #191919;
+}
+</style>
